7c8c9effc739ad12b555421a0d7346ca23533fd7
Moritz Lohrmann <moritz.lohrmann@stud.hs-bochum.de>
2026-08-18 16:28:41 +0200

    Einstellungen + Start

---
+++ Einstellungen.py
@@ -0,0 +1,29 @@
+from ursina import *
+
+
+class Einstellungen(Entity):
+    def __init__(self, **kwargs):
+        super().__init__(parent=camera.ui, **kwargs)
+
+        Text(
+            parent=self,
+            text='Einstellungen',
+            y=.11,
+            origin=(0, 0),
+            scale=1.5,
+        )
+
+        volume_slider = Slider(
+            0, 1,
+            default=Audio.volume_multiplier,
+            step=.05,
+            dynamic=True,
+            text='Lautstärke',
+            parent=self,
+            x=-.22,
+            y=-.02,
+        )
+
+        def set_volume(slider=volume_slider):
+            Audio.volume_multiplier = slider.value
+        volume_slider.on_value_changed = set_volume
--- Start.py
+++ Start.py
@@ -5,4 +5,3 @@ def main():
     hauptprogramm.start()
 if __name__ == "__main__":    
     main()
-
